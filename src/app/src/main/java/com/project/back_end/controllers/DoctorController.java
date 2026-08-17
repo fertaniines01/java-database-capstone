d63ab989c2aad219f70c36507614be70dcf20348
fertaniines01 <fertaniines01@gmail.com>
2026-08-17 02:19:53 +0200

    Create DoctorController.java

---
+++ src/app/src/main/java/com/project/back_end/controllers/DoctorController.java
@@ -0,0 +1,34 @@
+package com.project.back_end.controllers;
+
+import com.project.back_end.models.Doctor;
+import com.project.back_end.services.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping
+    public ResponseEntity<List<Doctor>> getAllDoctors() {
+        return ResponseEntity.ok(doctorService.getAllDoctors());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Doctor>> searchDoctors(
+            @RequestParam String specialty,
+            @RequestParam(required = false) String time) {
+        return ResponseEntity.ok(doctorService.searchDoctors(specialty, time));
+    }
+
+    @PostMapping
+    public ResponseEntity<Doctor> addDoctor(@RequestBody Doctor doctor) {
+        return ResponseEntity.ok(doctorService.saveDoctor(doctor));
+    }
+}
