24125ea82b04878ab78949aefe886bc9d9afc021
fertaniines01 <fertaniines01@gmail.com>
2026-08-17 02:21:20 +0200

    Create PrescriptionController.java

---
+++ src/app/src/main/java/com/project/back_end/controllers/PrescriptionController.java
@@ -0,0 +1,27 @@
+package com.project.back_end.controllers;
+
+import com.project.back_end.models.Prescription;
+import com.project.back_end.services.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/prescriptions")
+public class PrescriptionController {
+
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @GetMapping("/patient/{patientId}")
+    public ResponseEntity<List<Prescription>> getPrescriptionsByPatient(@PathVariable Long patientId) {
+        return ResponseEntity.ok(prescriptionService.getByPatientId(patientId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Prescription> createPrescription(@RequestBody Prescription prescription) {
+        return ResponseEntity.ok(prescriptionService.savePrescription(prescription));
+    }
+}
