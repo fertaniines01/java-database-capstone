71b39d395d443fe28a58562d3f855eef3a78c122
fertaniines01 <fertaniines01@gmail.com>
2026-08-18 17:37:58 +0200

    Update PatientRepository.java

--- src/app/src/main/java/com/project/back_end/repo/PatientRepository.java
+++ src/app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     Optional<Patient> findByEmail(String email);
+    Optional<Patient> findByEmailOrPhone(String email, String phone);
 }
