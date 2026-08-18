138326eaa2011fb6cf9595d717b1bc6ea161be2a
fertaniines01 <fertaniines01@gmail.com>
2026-08-18 17:34:45 +0200

    Update DoctorController.java

--- src/app/src/main/java/com/project/back_end/controllers/DoctorController.java
+++ src/app/src/main/java/com/project/back_end/controllers/DoctorController.java
@@ -2,33 +2,46 @@ package com.project.back_end.controllers;
 
 import com.project.back_end.models.Doctor;
 import com.project.back_end.services.DoctorService;
+import com.project.back_end.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/doctors")
+@RequestMapping("/doctors")
 public class DoctorController {
 
     @Autowired
     private DoctorService doctorService;
+
+    @Autowired
+    private TokenService tokenService;
 
     @GetMapping
     public ResponseEntity<List<Doctor>> getAllDoctors() {
         return ResponseEntity.ok(doctorService.getAllDoctors());
     }
 
-    @GetMapping("/search")
-    public ResponseEntity<List<Doctor>> searchDoctors(
-            @RequestParam String specialty,
-            @RequestParam(required = false) String time) {
-        return ResponseEntity.ok(doctorService.searchDoctors(specialty, time));
-    }
-
     @PostMapping
     public ResponseEntity<Doctor> addDoctor(@RequestBody Doctor doctor) {
-        return ResponseEntity.ok(doctorService.saveDoctor(doctor));
+        return ResponseEntity.status(HttpStatus.CREATED).body(doctorService.saveDoctor(doctor));
+    }
+
+    @GetMapping("/availability/{user}/{doctorId}/{date}/{token}")
+    public ResponseEntity<?> getDoctorAvailability(
+            @PathVariable String user,
+            @PathVariable Long doctorId,
+            @PathVariable String date,
+            @PathVariable String token) {
+        
+        if (!tokenService.validateToken(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Token");
+        }
+        
+        List<String> availabilities = doctorService.getDoctorAvailability(doctorId, date);
+        return ResponseEntity.ok(availabilities);
     }
 }
